d4d7b011238db2c759985e891bd1b7f1b593dfb5
Vassallo Nicolás <42422704+NikozV@users.noreply.github.com>
2022-03-26 12:27:00 -0300

    template inicio, menu de nav

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.inicio, name='inicio'),
-    path('nosotros', views.nosotros, name='nosotros'),
-}
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('libros/', views.libros, name='libros'),
+]
