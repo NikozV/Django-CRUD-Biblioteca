1e7dc0e2391dc3cc26ed51e19ff86b9f7cd087d6
Vassallo Nicolás <42422704+NikozV@users.noreply.github.com>
2022-03-28 22:58:24 -0300

    importo models a admin, creo super usuario

--- libreria/models.py
+++ libreria/models.py
@@ -7,4 +7,4 @@ class Libro(models.Model):
     id= models.AutoField(primary_key=True)
     titulo = models.CharField(max_length=100, verbose_name="Título")
     imagen = models.ImageField(upload_to='imagenes/', verbose_name="Imagen", null=True)
-    description = models.TextField(verbose_name= "Descripción", null=True)
+    descripcion = models.TextField(verbose_name= "Descripción", null=True)
